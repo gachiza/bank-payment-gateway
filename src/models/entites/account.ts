faf2c21cb9ed2b96ba1146db14006d528742edb0
Gachiza <gachiza17@gmail.com>
2025-06-10 14:38:02 +0200

    setup accounts model

---
+++ src/models/entites/account.ts
@@ -0,0 +1,34 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, ManyToOne } from "typeorm";
+import { User } from "./User";
+import { Transaction } from "./Transaction";
+
+
+@Entity()
+export class Account {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ unique: true})
+  accountNumber: string;
+
+  @Column()
+  password: string;
+
+  @Column({ type: "decimal", precision: 15, scale: 2, default: 0})
+  balance: Number;
+
+  @Column()
+  currency: string;
+
+  @ManyToOne(() => User, (user) => user.accounts)
+  user: User;
+
+  @OneToMany(() => Transaction, (transactions) => transaction.account)
+  transactions: Transaction[];
+
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP"})
+  createdAt: Date;
+
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP"})
+  updatedAt: Date;
+}
