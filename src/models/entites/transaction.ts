ea3341e723f66c694e1ace796ebf9f40baa8ea0f
Gachiza <gachiza17@gmail.com>
2025-06-10 14:51:17 +0200

    transaction setup

---
+++ src/models/entites/transaction.ts
@@ -0,0 +1,45 @@
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { Account } from "./Account";
+
+export enum TransactionType {
+  DEPOSIT= "deposit",
+  WITHDRAW= "withdrawal",
+  TRANSFER= "transfer",
+  PAYMENT= "payment"
+}
+
+
+@Entity()
+export class Transaction {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column({ type: "decimal", precision: 15, scale: 2, default: 0})
+  amount: Number;
+
+
+  @Column()
+  currency: string;
+
+  @ManyToOne(() => User, (user) => user.accounts)
+  user: User;
+
+  @Column({ type: "enum", enum: TransactionType})
+  type: TransactionType;
+
+  @Column({nullable: true})
+  description: string;
+
+  @Column({ nullable: true})
+  referemce: string;
+
+  @Column({ default: "pending"})
+  status: string;
+
+  @ManyToOne(() => Account, (account) => account.transactions)
+  account: Account;
+
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP"})
+  createdAt: Date;
+
+}
