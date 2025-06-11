ac9e49423110386a004ef51d11643b0073e699c1
Gachiza <gachiza17@gmail.com>
2025-06-11 11:22:04 +0200

    server setup

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import { createApp } from "./app";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+  const app = await createApp();
+
+  app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+  });
+};
+
+startServer().catch((error) => {
+  console.log("Failed to start server:", error);
+  process.exit(1);
+});
