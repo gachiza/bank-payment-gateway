f27d26c31bb0fbc86b550151fb69a6849a50ff60
Gachiza <gachiza17@gmail.com>
2025-06-11 09:39:58 +0200

    auth.middleware setup

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,34 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { AuthService } from "../services/auth.services";
+import { ApiResponse } from "../utils/apiResponse";
+
+const JWT_SECRET = process.env.JWT_SECRET || " your-secret-key";
+
+declare module "express" {
+  interface Request {
+    user?: any;
+  }
+}
+
+export const authMiddleware = {
+  authenticate: (req: Request, res:Response, next: NextFunction) => {
+    const token = req.header("Authorization")?.replace("Bearer", "");
+    if (!token ){
+      return ApiResponse.error (res, "Authentication required", 401);
+    }
+    try {
+      const decoded = jwt.verify(token, JWT_SECRET);
+      req.user = decoded;
+      next();
+    } catch (error) {
+      return ApiResponse.error(res, "Invalid token ", 401);
+    }
+  },
+  authorizeAdmin: (req: Request, res: Response, next:NextFunction) => {
+    if (!req.user?.isAdmin) {
+      return ApiResponse.error(res, "Admin Access Required", 403);
+    }
+    next();
+  }
+};
