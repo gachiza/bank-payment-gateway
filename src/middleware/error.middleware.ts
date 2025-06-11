98fac9efa613c8aefe408cebd4a7f68315a35111
Gachiza <gachiza17@gmail.com>
2025-06-11 14:23:11 +0200

    error middleware setup

---
+++ src/middleware/error.middleware.ts
@@ -0,0 +1,12 @@
+import { Request, Response, NextFunction } from "express";
+import { ApiResponse } from "../utils/apiResponse";
+
+export const errorMiddleware = (
+  error: Error,
+  req:Request,
+  res:Response,
+  next: NextFunction
+) => {
+  console.error(error.stack);
+  return ApiResponse.error(res, " Internal server error", 500);
+};
